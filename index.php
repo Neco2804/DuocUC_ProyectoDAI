a8aab25204f7e157b4bfaefd57d29d5294ccbc06
Gustavo <gustavo.bitterly@outlook.com>
2019-05-27 21:45:03 -0400

    Directorios
    Se actualizan directorios

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="\dai\Views\login.html">Ingreso</a>
+            <a class="nav-link" href="#">Ingreso</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Registro</a>
