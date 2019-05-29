ab4d80c103196aaf1640cee8a5d7864726fa2739
René <rene.parada@icloud.com>
2019-05-29 17:24:27 -0400

    ordene el codigo en c/u

--- Models/client.php
+++ Models/client.php
@@ -3,23 +3,22 @@
     class Client{
         var $RUT;
 
-    function __construct($RUT){
-        $this->RUT = $RUT;
-    }
+        function __construct($RUT){
+            $this->RUT = $RUT;
+        }
         
-    function save(){
-        $sql = "INSERT INTO client (RUT)
-                VALUES ('".$this->RUT."')";
-    
-    $link = connect();
-    if ($link->query($sql) === TRUE ){
-        return $this;
-    } else {
-        echo "Error: " . $sql . "<br>" . $link->error;
-        return false;
-    }
-    }
-
+        function save(){
+            $sql = "INSERT INTO client (RUT)
+                    VALUES ('".$this->RUT."')";
+        
+        $link = connect();
+            if ($link->query($sql) === TRUE ){
+                return $this;
+            } else {
+                echo "Error: " . $sql . "<br>" . $link->error;
+                return false;
+            }
+        }
     }
 
 ?>
--- Models/worker.php
+++ Models/worker.php
@@ -4,24 +4,23 @@
         var $RUT;
         var $COD_PROFESSION;
 
-    function __construct($RUT, $COD_PROFESSION){
-        $this->RUT = $RUT;
-        $this->COD_PROFESSION = $COD_PROFESSION;
-    }
+        function __construct($RUT, $COD_PROFESSION){
+            $this->RUT = $RUT;
+            $this->COD_PROFESSION = $COD_PROFESSION;
+        }
         
-    function save(){
-        $sql = "INSERT INTO worker (RUT, COD_PROFESSION)
-                VALUES ('".$this->RUT."', '".$this->COD_PROFESSION."')";
-    
-    $link = connect();
-    if ($link->query($sql) === TRUE ){
-        return $this;
-    } else {
-        echo "Error: " . $sql . "<br>" . $link->error;
-        return false;
-    }
-    }
-
+        function save(){
+            $sql = "INSERT INTO worker (RUT, COD_PROFESSION)
+                    VALUES ('".$this->RUT."', '".$this->COD_PROFESSION."')";
+        
+        $link = connect();
+            if ($link->query($sql) === TRUE ){
+                return $this;
+            } else {
+                echo "Error: " . $sql . "<br>" . $link->error;
+                return false;
+            }
+        }
     }
 
 ?>
