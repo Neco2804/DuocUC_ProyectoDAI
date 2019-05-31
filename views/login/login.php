e1a587e1dbb5090cd9d5606c5bb40d457a1921f4
Gustavo <gustavo.bitterly@outlook.com>
2019-05-30 21:46:04 -0400

    Update login.php
    Se actualiza codigo de login

--- views/login/login.php
+++ views/login/login.php
@@ -9,25 +9,18 @@ include_once 'connect.php';
 
 //Comprobar de envío el formulario
 if (isset($_POST['login'])) {
-
-	$email = mysqli_real_escape_string($con, $_POST['email']);
-	$password = mysqli_real_escape_string($con, $_POST['password']);
-	$result = mysqli_query($con, "SELECT * FROM users WHERE email = '" . $email. "' and password = '" . md5($password) . "'");
+	$link = connect();
+	$email = mysqli_real_escape_string($link, $_POST['email']);
+	$password = mysqli_real_escape_string($link, $_POST['password']);
+	$result = mysqli_query($link, "SELECT * FROM user WHERE EMAIL = '" . $email. "' and PASSWORD = '" . $password . "'");
 
 	if ($row = mysqli_fetch_array($result)) {
-		//$_SESSION['usr_estado'] = $row['estado'];
-
-		if($row['estado']==1){
-			$_SESSION['usr_id'] = $row['id'];
-			$_SESSION['usr_name'] = $row['name'];
-			
-			header("Location: index.php");
-		}else
-		$errormsg = "Esta cuenta esta desactivada";
-	} else {
-		$errormsg = "Revisa los datos!!!";
+			echo "Usuario conectado correctamente";
+		}else {
+		$errormsg = "Datos incorrectos";
 	}
 }
+
 ?>
 <html>
 <head>
@@ -39,48 +32,41 @@ if (isset($_POST['login'])) {
 
 </head>
 
-<nav class="navbar navbar-invisible">
+	<nav class="navbar navbar-invisible"></nav>
 
-    </nav>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4 well">
+				<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
+					<fieldset>
 
+						<div class="form-group">
+							<label for="name">E-mail</label>
+							<input type="text" name="email" placeholder="Ingresar Email" required class="form-control" />
+						</div>
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4 well">
-			<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
-				<fieldset>
-					<legend>Login</legend>
-					<!--div class="form-group clearfix">
-						<img src="http://www.iconsfind.com/wp-content/uploads/2016/10/20161014_58006bff8b1de.png" alt="" width="200px" class="img-responsive img-circle" style="margin:0 auto">
-					</div-->
+						<div class="form-group">
+							<label for="name">Contraseña</label>
+							<input type="password" name="password" placeholder="Ingresar Contraseña" required class="form-control" />
+						</div>
 
-					<div class="form-group">
-						<label for="name">E-mail</label>
-						<input type="text" name="email" placeholder="Ingresar Email" required class="form-control" />
-					</div>
-
-					<div class="form-group">
-						<label for="name">Contraseña</label>
-						<input type="password" name="password" placeholder="Ingresar Contraseña" required class="form-control" />
-					</div>
-
-					<div class="form-group">
-						<input type="submit" name="login" value="Iniciar Sesion" class="btn btn-primary" />
-						<input type="reset" value="Limpiar" class="btn btn-default" >
-					</div>
-				</fieldset>
-			</form>
-			<span class="text-danger"><?php if (isset($errormsg)) { echo $errormsg; } ?></span>
+						<div class="form-group">
+							<input type="submit" name="login" value="Iniciar Sesion" class="btn btn-primary" />
+							<input type="reset" value="Limpiar" class="btn btn-default" >
+						</div>
+					</fieldset>
+				</form>
+				<span class="text-danger"><?php if (isset($errormsg)) { echo $errormsg; } ?></span>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4 text-center">	
+			No tienes cuenta? <a href="register.php">Regitrate aqui</a>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4 text-center">	
-		No tienes cuenta? <a href="register.php">Regitrate aqui</a>
-		</div>
-	</div>
-</div>
 
-<script src="js/jquery-1.10.2.js"></script>
-<script src="js/bootstrap.min.js"></script>
-</body>
+	<script src="js/jquery-1.10.2.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	</body>
 </html>
