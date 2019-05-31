f888401a8d7bde3622033e2b2bd1ee6e012266bc
Oscar Lobos Donoso <43558082+htmlrlzd00d@users.noreply.github.com>
2019-05-30 22:14:36 -0400

    Connect y Testeando pal René
    Archivos para tarea Homologación de René

---
+++ Test/connect.php
@@ -0,0 +1,21 @@
+<?php
+
+    function connect(){
+
+        $database_name = "dbproyecto";
+        $username = "dbproyecto";
+        $password = "dbproyecto";
+        $server = "localhost";
+
+        $link = mysqli_connect($server, $database_name, $username, $password);
+
+        if (!$link) {
+                echo "Error: Unable to connect to MySQL." . PHP_EOL;
+                echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+                echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+                return false;
+        }else{
+            return $link;
+        }
+    }
+?>
