2e79757be2940552d167278a25230731ec505148
Oscar Lobos Donoso <43558082+htmlrlzd00d@users.noreply.github.com>
2019-05-31 14:58:42 -0400

    Update testeando.php

--- Test/testeando.php
+++ Test/testeando.php
@@ -3,7 +3,8 @@
 include 'connect.php';
 include 'C:\xampp\htdocs\dai\models\commune.php';
 
-$comuna_lp = Commune::all_communes();
+$data_lp = array('COD_COMUNNE'=>'1', 'COD_NAME_COMM'=>'Pedro Aguirre Cerda');
+$lo_prado = new Commune($data_lp);
+$lo_prado->save();
 
-echo count($comuna_lp);
 ?>
