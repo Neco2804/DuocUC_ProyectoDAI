614d93dc1028e7488c9ee8c6290c3bf5f3de5543
Oscar Lobos Donoso <43558082+htmlrlzd00d@users.noreply.github.com>
2019-06-02 16:01:23 -0400

    VIsta Ingreso Trabajador Ready
    Modelo de Professions listo

--- Models/professions.php
+++ Models/professions.php
@@ -1,11 +1,39 @@
 <?php
 
     class Professions{
-        var $id;
+        var $COD_PROFESSION;
         var $NAME_PROFESSION;
 
-        function __construct($NAME_PROFESSION) {
-        $this->NAME_PROFESSION = $NAME_PROFESSION;
+        function __construct($data) {
+        $this->COD_PROFESSION = $data['COD_PROFESSION'];
+        $this->NAME_PROFESSION = $data['NAME_PROFESSION'];
+        }
+
+        static function all_professions(){
+            $query = "SELECT COD_PROFESSION, NAME_PROFESSION FROM professions;";
+            $link = connect();
+            $result = $link->query($query);
+            $resp = array();
+
+            while($row = $result->fetch_assoc()){
+                $prof = new Professions($row);
+                array_push($resp, $prof);
+            }
+
+            return $resp;
+        }
+
+        static function get_profession($COD_COMUNNE){
+            $query = "SELECT * FROM professions WHERE COD_PROFESSION=".$COD_PROFESSION.";";
+            $link = connect();
+            $result = $link->query($query);
+            $resp = array();
+
+        while($row = $result->fetch_assoc()) {
+            $prof = new Professions($row);
+            array_push($resp, $prof);
+        }
+           return $resp[0];
         }
 
         function save(){
@@ -22,6 +50,21 @@
                 return false;
             }
         }
+
+        function delete(){
+            $sql = "DELETE FROM professions where COD_PROFESSION = ".$this->COD_PROFESSION.";";
+            $link = connect();
+            mysqli_query($link, $sql);
+        }
+    
+        function update(){
+            $sql = "UPDATE professions SET COD_PROFESSION='".$this->COD_PROFESSION."' WHERE COD_PROFESSION=".$this->COD_PROFESSION;
+            $link = connect();
+            mysqli_query($link, $sql);
+        }
+
+
+
     }
 
 ?>
