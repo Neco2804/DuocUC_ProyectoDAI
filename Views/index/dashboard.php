045da6e87a731b61611f00112bc46f0cd215e917
Oscar Lobos Donoso <43558082+htmlrlzd00d@users.noreply.github.com>
2019-06-02 16:31:04 -0400

    Movimiento de Carpetas

---
+++ Views/index/dashboard.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Ayuda.ME Dashboard</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport" >
+	<link rel="stylesheet" href="http://localhost/dai/Views/index/css/bootstrap.min.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
+
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <?php include('css/dashboard_css.php')?>
+
+</head>
+
+	<nav class="navbar navbar-invisible"></nav>
+
+	
+	</div>
+
+	<script src="http://localhost/dai/Views/index/js/jquery-1.10.2.js"></script>
+	<script src="http://localhost/dai/Views/index/js/bootstrap.min.js"></script>
+	</body>
+</html>
