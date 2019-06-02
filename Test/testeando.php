9218b5fac7189cba462d51257335e3c2b51b06b9
Gustavo <gustavo.bitterly@outlook.com>
2019-06-02 19:48:39 -0400

    Update testeando.php

--- Test/testeando.php
+++ Test/testeando.php
@@ -5,9 +5,11 @@ include 'C:\xampp\htdocs\dai\models\professions.php';
 include 'C:\xampp\htdocs\dai\models\user.php';
 include 'C:\xampp\htdocs\dai\models\client.php';
 
+/*
 $datos = array('RUT'=>'12345679','DV'=>'9','NAME'=>'Pepe','LAST_NAME'=>'Tapia','DIRECTION'=>'Las Rejas 1', 'COMMUNE'=>'1', 'EMAIL'=>'pepe@tapia.cl', 'PASSWORD'=>'admin', 'TYPE_USER'=>'1', 'RATING'=>' ');
 $persona = new User($datos);
 $persona->save();
-echo $persona->TYPE_USER;
+$persona->TYPE_USER;
+*/
 
 ?>
