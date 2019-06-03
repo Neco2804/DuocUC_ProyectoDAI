50add6fed1aacd3a12a9976cf498f820193acaa4
Oscar Lobos Donoso <43558082+htmlrlzd00d@users.noreply.github.com>
2019-06-02 20:41:36 -0400

    Dashboard listo

--- Views/index/dashboard.php
+++ Views/index/dashboard.php
@@ -27,17 +27,18 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Ayuda.Me</a>
+      <a class="navbar-brand" href="#">Ayuda.Me - Bienvenid@</a>
+      <a class="navbar-brand" href="#"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#">Ingreso</a>
+            <a class="nav-link" href="#"></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Registro</a>
+            <a class="nav-link" href="#">Salir</a>
           </li>
         </ul>
       </div>
